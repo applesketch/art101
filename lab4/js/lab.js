66cf52e2c67d90a717bf69e0cd1ab7581d18975e
applesketch <applezsketch@gmail.com>
2022-10-12 12:59:20 -0700

    Lab 4 changes
    push time

---
+++ lab4/js/lab.js
@@ -0,0 +1,54 @@
+// Summary. (use period)
+// Description. (use period)
+
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+
+
+// How to have a decent sleeping schedule.
+  // Get ready for bed at a certain time.
+    // Recommended: According to https://health.clevelandclinic.org/what-time-should-i-go-to-bed/, the optimal sleeping time is 10PM.
+    // Take a shower, brush your teeth, do whatever skincare routine you need before 10PM.
+  // Set an alarm for the next day
+    // Recommended: According to https://www.womanandhome.com/health-wellbeing/best-time-to-sleep-and-wake-up/#:~:text=The%20best%20time%20to%20wake,the%20body's%20processes%20to%20work. the optimal time to wake up is from 7AM to 9AM to get seven to nine hours of sleep.
+  // Get on the bed, lay down and close your eyes.
+    // If you can’t fall to sleep easily, some tips to help your sleep better include:
+      // Find a comfortable position.
+      // Drinking warm milk.
+      // Have a good sleeping environment - setting your thermostat around 68 degrees and having white noise (such a fan, Youtube rain noises video, etc.) in the background may help.
+      // Close all lights and curtains for optimal darkness.
+      // Turn off all electronics.
+  // Keep sleep schedules consistent for optimal productivity and energy.
+
+
+//How to open your FREE and OPEN SOURCE art software (Krita).
+
+// Go to https://krita.org/en/download/krita-desktop/
+// Click the huge, blue download button that says, “Get Krita Now” on the right side of the screen.
+// Depending on your hardware:
+  // Windows - Click on “Windows Installer 64-bit”
+    // If you need the Windows 32-bit version, scroll down where it says “Download Older Versions” and click on the “Windows 32-bit version” button.
+  // macOS - Click on “All Download Versions” under the blue button and click on “macOS Installer”
+  // Linux - Click on “All Download Versions” under the blue button and click on “Linux 64-bit Appimage”
+// Wait for the software installation package to download
+  // Look for the installation package in your in your file directory (downloads folder)
+  // Click ‘yes’ when prompted to allow changes on your computer.
+  // Choose your preferred language.
+  // When you see the setup page, click next to continue.
+// Accept the terms of the License Agreement and click next to continue.
+// Install and Run the application.
+  // Choose your desired destination folder for the application to appear in.
+    // Allow desktop shortcut for the icon to appear on your desktop
+  // Optional: Keep all check boxes and agree to allow Krita shell to install
+// Chose a Start Menu Folder
+  // Recommended: Keep it as ‘Krita’ and click next
+// Confirm Installation
+  // Recommended: Keep checkbox checked for creating a desktop icon
+// Click install and wait for the installation to complete.
+  //  When the installation is completed, click next to continue.
+  // Finally, click Finish.
+// Go to your desktop and double click the Krita application.
+// Enjoy your new, shiny free and open source drawing software!
